679fd367f46e5755fc044e293b65ad10047eb1a2
新垣陸 <shingakiriku@aragakirikunoMacBook-Air.local>
2019-01-06 13:44:37 +0900

    second main.js

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 const lastName = "新垣";
 const firstName = "陸";
-const fullName = lastName + firstName;
+const fullName = lastName + ' ' + firstName;
+
 const myAge = 20;
 const friendAge = 22;
 const totalAge = myAge + friendAge;
 const number0fDays = totalAge * 365;
+
 const number0fChildren = 3;
 const number0fCakes = 10;
 const number0fCakesByChild = number0fCakes / number0fChildren;
 const number0fRest0fCakes = number0fCakes % number0fChildren;
+
+console.log(lastName)
+console.log(firstName)
+console.log(fullName)
+
+console.log(myAge)
+console.log(friendAge)
+console.log(totalAge)
+console.log(number0fDays)
+
+console.log(number0fChildren)
+console.log(number0fCakes)
+console.log(number0fCakesByChild)
+console.log(number0fRest0fCakes)
